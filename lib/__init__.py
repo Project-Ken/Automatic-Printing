7c69ce3e08b38cc43e3dd96085afad3962059aa8
Project-Ken <ktsiu.ken@gmail.com>
2026-08-08 03:49:36 +0800

    Initialize Library #1

---
+++ lib/__init__.py
@@ -0,0 +1,19 @@
+import os
+import re
+import sys
+import json
+import time
+import ctypes
+import shutil
+import socket
+import datetime
+import platform
+import threading
+import subprocess
+
+
+
+import mysql.connector as MySQL
+from mysql.connector import Error as MySQLError
+import oracledb as OracleSQL
+import psycopg2 as PostgreSQL
