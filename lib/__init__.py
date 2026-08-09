ae3583102b2344b183de27b8f3c23c8617383968
Project-Ken <ktsiu.ken@gmail.com>
2026-08-09 18:03:17 +0800

    Initialize Library #3
    Initialize Database Object

--- lib/__init__.py
+++ lib/__init__.py
@@ -16,3 +16,16 @@ import subprocess
 import mysql.connector as MySQL_Connector
 import oracledb as OracleSQL
 import psycopg2 as PostgreSQL
+
+from .Database import Info as DatabaseInfo
+from .Database import PostgreSQL_Database
+
+StatusDatabase = PostgreSQL_Database(
+    DatabaseInfo(
+        host = '127.0.0.1',
+        port = '5432',
+        database = 'service',
+        user = 'service_handler',
+        password = 'P@zzw0rd'
+    )
+)
