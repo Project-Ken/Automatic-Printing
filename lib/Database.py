cdc30e9e7806f2ed508c4cd8fb4fb691db654acc
Project-Ken <ktsiu.ken@gmail.com>
2026-08-09 17:50:28 +0800

    Database Class #2
    Common Function
    * Connect
    * Disconnect
    * Rollback
    * Commit
    * Execute
    * ExecuteCommit

--- lib/Database.py
+++ lib/Database.py
@@ -12,3 +12,50 @@ class Base:
         if cls is Base:
             raise TypeError('Base Class Cannot be Instantiated Directly')
         return super().__new__(cls)
+
+    def Connect(self):
+        raise NotImplementedError('Base Class Function [ Connect ] Not For Use')
+
+    def Disconnect(self):
+        if self.connection:
+            self.connection.close()
+            self.connection = None
+            print(' - Database Disconnected')
+
+    def Rollback(self):
+        if self.connection:
+            self.connection.rollback()
+
+    def Commit(self):
+        if self.connection:
+            self.connection.commit()
+
+    def Execute(self, sql:str, *args):
+        connection = self.Connect()
+        cursor = None
+        try:
+            cursor = connection.cursor()
+            cursor.execute(sql, args)
+
+            return cursor.fetchall()
+        except Exception as e:
+            print(f'SQL: {sql}\n - SQL Error\n{str(e)}')
+            self.Rollback()
+            return None
+        finally:
+            if cursor:
+                cursor.close()
+
+    def ExecuteCommit(self, sql:str, *args):
+        connection = self.Connect()
+        cursor = None
+        try:
+            cursor = connection.cursor()
+            cursor.execute(sql, args)
+            self.Commit()
+        except Exception as e:
+            print(f'SQL: {sql}\n- SQL Error\n{str(e)}')
+            self.Rollback()
+        finally:
+            if cursor:
+                cursor.close()
